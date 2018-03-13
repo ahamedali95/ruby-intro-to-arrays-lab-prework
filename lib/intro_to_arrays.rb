4c990ae854302e27894b50fa0ec48fbd199dae16
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-13 01:38:27 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,3 +1,16 @@
 def instantiate_new_array()
   new_arr = []
 end
+
+def array_with_two_elements()
+  arr = []
+  i = 0
+
+  loop do
+    arr.push(i)
+    i += 1
+    break if i == 2
+  end
+
+  return arr
+end
